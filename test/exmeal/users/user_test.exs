a4cff62c10b0d7c9651dba1f6dcf27f76284419e
Helder Couto <helder@brassun.com.br>
2022-01-28 21:59:54 -0300

    Adicionado factory e testes de user

---
+++ test/exmeal/users/user_test.exs
@@ -0,0 +1,26 @@
+defmodule Exmeal.Users.UserTest do
+  use Exmeal.DataCase
+
+  import Exmeal.Factory
+
+  alias Ecto.Changeset
+
+  alias Exmeal.Users.User
+
+  describe "changeset/1" do
+    test "when all params are valid, returns a valid changeset" do
+      params = build(:users_params)
+
+      response = User.changeset(params)
+
+      assert %Changeset{
+               changes: %{
+                 cpf: "12345678900",
+                 name: "Jp",
+                 email: "jp@banana.com"
+               },
+               valid?: true
+             } = response
+    end
+  end
+end
